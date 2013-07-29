c816a1a48b881676f70168d9b4973d94e27c5334
Evan Kuhn <evan@brightroll.com>
2013-07-28 20:41:38 -0700

    finish chapter 5 exercises

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,1 @@
-def page_title(page_name = '')
-  base_title = "Ruby on Rails Tutorial Sample App"
-  (page_name.empty? ? base_title : base_title + " | #{page_name}")
-end
-
+include ApplicationHelper
